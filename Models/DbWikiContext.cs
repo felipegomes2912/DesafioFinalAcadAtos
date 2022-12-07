1f5ebdc1a26e7a829be860481df7a9352ca2b1ca
felipegomes2912 <filifirme@gmail.com>
2022-12-07 00:18:29 -0300

    Atualização projeto final

---
+++ Models/DbWikiContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoFinalAcadNet.Models;
+
+public partial class DbWikiContext : DbContext
+{
+    public DbWikiContext()
+    {
+    }
+
+    public DbWikiContext(DbContextOptions<DbWikiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Instrumento> Instrumentos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost;initial Catalog=dbWiki;User ID=dbWiki;password=dbWiki;language=Portuguese;Trusted_Connection=True;TrustServerCertificate=True;").UseLazyLoadingProxies();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Instrumento>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__instrume__3213E83FE22BCF64");
+
+            entity.ToTable("instrumento");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Cor)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("cor");
+            entity.Property(e => e.Corda).HasColumnName("corda");
+            entity.Property(e => e.DataFabricado)
+                .HasColumnType("date")
+                .HasColumnName("dataFabricado");
+            entity.Property(e => e.Descricao)
+                .HasMaxLength(250)
+                .IsUnicode(false)
+                .HasColumnName("descricao");
+            entity.Property(e => e.Marca)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("marca");
+            entity.Property(e => e.Nome)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nome");
+            entity.Property(e => e.Percursao).HasColumnName("percursao");
+            entity.Property(e => e.Peso).HasColumnName("peso");
+            entity.Property(e => e.QntdEstoque).HasColumnName("qntdEstoque");
+            entity.Property(e => e.Sopro).HasColumnName("sopro");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Instrumento.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoFinalAcadNet.Models;
+
+public partial class Instrumento
+{
+    [Column("id")]
+    [Display(Name = "Código")]
+    public int Id { get; set; }
+
+    [Column("nome")]
+    [Display(Name = "Nome")]
+    public string Nome { get; set; } = null!;
+
+    [Column("marca")]
+    [Display(Name = "Marca")]
+    public string Marca { get; set; } = null!;
+
+    [Column("descricao")]
+    [Display(Name = "Modelo")]
+    public string? Descricao { get; set; }
+
+    [Column("cor")]
+    [Display(Name = "Cor")]
+    public string? Cor { get; set; }
+
+    [Column("peso")]
+    [Display(Name = "Peso (g)")]
+    public int Peso { get; set; }
+
+    [Column("dataFabricado")]
+    [Display(Name = "Data de registro no sistema")]
+    public DateTime DataFabricado { get; set; }
+
+    [Column("qntdEstoque")]
+    [Display(Name = "Quantidade em estoque")]
+    public int QntdEstoque { get; set; }
+
+    [Column("sopro")]
+    [Display(Name = "Sopro")]
+    public bool Sopro { get; set; }
+
+    [Column("corda")]
+    [Display(Name = "Corda")]
+    public bool Corda { get; set; }
+
+    [Column("percursao")]
+    [Display(Name = "Percursão")]
+    public bool Percursao { get; set; }
+}
